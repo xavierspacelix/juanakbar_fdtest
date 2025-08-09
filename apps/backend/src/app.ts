d9256916094891f0f7bdbde28a75c0299c2b134c
xavierspacelix <juanakbar361@gmail.com>
2025-08-09 11:44:07 +0700

    feat(api): serve uploads folder for static image access

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -7,10 +7,11 @@ import authRoutes from "./routes/auth.routes";
 import bookRoutes from "./routes/book.routes";
 import userRoutes from "./routes/user.routes";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.APP_FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -26,4 +27,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/users", userRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 export default app;
